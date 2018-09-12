6e64578c44a8f81ea358ece2d1e27dbfa2406898
jay <jlaura@usgs.gov>
2018-09-12 13:52:34 -0700

    Removes app

--- app/__init__.py
+++
@@ -1,1 +0,0 @@
-__name__ = 'KRC_Interpolator'
--- app/examples/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import app
-
-__all__ = ['get_path']
-
-def get_path(example_name):
-    base = os.path.split(app.__file__)[0]
-    return os.path.join(base, 'examples', example_name)
